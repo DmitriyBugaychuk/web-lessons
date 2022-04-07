2c77b01bf9132b7b2a81cb7ffaa7958a4bf2ebac
Dmitriy <dimabugaychuk200316@gmail.com>
2022-04-07 19:49:19 +0300

    add homework

---
+++ 08/script.js
@@ -0,0 +1,20 @@
+function homework1(){
+document.write('Моє чудове місто - Вишгород');
+}
+
+function homework2(){
+document.write('<i> Я навчаюся в коледжі </i>');
+}
+
+function homework3(){
+let str = '<i> Мій улюблений футбольний клуб - Арсенал </i>';
+document.write(str);
+}
+
+function homework4(){
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
+}
